4de94c1281b514e704facd74cd3ed3a76ad23483
Mahnoor Waseem <mahnoorwaseem60@gmail.com>
2024-12-20 16:27:29 +0500

    employee dashboard added

---
+++ src/others/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='flex justify-between'>
+      <h1 className='text-2xl font-medium '>Hi <br /> <span className='text-3xl font-semibold'>Mahnoor</span></h1>
+      <button className='bg-red-700 text-white font-bold px-5 py-3 rounded-xl text-lg'>Log Out</button>
+    </div>
+  )
+}
+
+export default Header
+ 
